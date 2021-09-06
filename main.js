eda3c6289c89b9dafcae95f1e7d941ec9b9be729
cristian-santana <cs824454@gmail.com>
2021-09-06 12:03:42 -0300

    cambie imagenes y retoque la galeria de proyectos

--- main.js
+++ main.js
@@ -14,3 +14,18 @@ if (valor = "true"){
 } else {
     body.classList.remove("dark")
 }
+
+
+
+const $form = document.querySelector('#form')
+const $buttonMailto = document.querySelector('#trucazo')
+
+$form.addEventListener('submit',handleSubmit)
+
+function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    console.log(form.get('name'))
+    $buttonMailto.setAttribute('hret','mailto:cs824454@gmail.com?subject=nombre ${form.get(.name)} correo ${form.get(.email)}&body=${form.get(.message)}')
+    $buttonMailto.click()
+}
